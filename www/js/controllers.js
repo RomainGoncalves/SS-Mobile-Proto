a9321021a55430ac6b08f49118ffffa3e8a5472c
Romain Goncalves <gotthefevermedia@gmail.com>
2013-12-19 14:45:35 +0100

    Tested out publications

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -13,4 +13,51 @@ function PublicationsCtrl($scope, SessionService){
 	
 	$scope.username = SessionService.get('username') ;
 
+	var publications = [
+		{ 
+			body: 'Lorem ipsum',
+			user_id: '1',
+			date: '10/08/2012',
+			comments:[
+				{
+					body: 'Coment 1',
+					user_id: '12',
+					date: '10/08/2112'
+				},
+				{
+					body: 'Coment 2',
+					user_id: '32',
+					date: '10/08/2532'
+				},
+				{
+					body: 'Coment 3',
+					user_id: '42',
+					date: '10/08/2112'
+				}
+			]
+
+		},
+		{ 
+			body: 'Lorem ipsum 2',
+			user_id: '2',
+			date: '10/08/2112',
+			comments:[
+				{
+					body: 'Coment 1',
+					user_id: '12',
+					date: '10/08/2112'
+				}
+			]
+
+		},
+		{ 
+			body: 'Lorem ipsum 3',
+			user_id: '3',
+			date: '10/08/2212'
+
+		}
+	];
+
+	$scope.publications = publications;
+
 }
--- www/js/main.js
+++ www/js/main.js
@@ -13,31 +13,32 @@ app.config(['$routeProvider', '$locationProvider', function($routeProvider, $loc
 
 }]);//end config
 
+//Probably temporary service for inserting data in session storage - HTML5
 app.factory('SessionService', function(){
-		return {
-			get: function(key){
+	return {
+		get: function(key){
 
-				//Check first if the browser has sessionStorage
-				if(Modernizr.sessionstorage){
-					return sessionStorage.getItem(key) ;
-				}
-
-			},
-			set: function(key, value){
-
-				//Check first if the browser has sessionStorage
-				if(Modernizr.sessionstorage){
-					return sessionStorage.setItem(key, value) ;
-				}
-
-			},
-			unset: function(key){
-
-				//Check first if the browser has sessionStorage
-				if(Modernizr.sessionstorage){
-					return sessionStorage.removeItem(key) ;
-				}
-
+			//Check first if the browser has sessionStorage
+			if(Modernizr.sessionstorage){
+				return sessionStorage.getItem(key) ;
 			}
+
+		},
+		set: function(key, value){
+
+			//Check first if the browser has sessionStorage
+			if(Modernizr.sessionstorage){
+				return sessionStorage.setItem(key, value) ;
+			}
+
+		},
+		unset: function(key){
+
+			//Check first if the browser has sessionStorage
+			if(Modernizr.sessionstorage){
+				return sessionStorage.removeItem(key) ;
+			}
+
 		}
-	})
+	}
+})
